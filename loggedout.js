1d4a0a3e9d851257c942cf5cfe4105580bde75c8
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-04 14:47:07 +0300

    Update loggedout.js

--- loggedout.js
+++ loggedout.js
@@ -9,6 +9,6 @@ let okdesk_link = document.createElement('span');
 okdesk_link.innerHTML = '<a href="https://trans-control.okdesk.ru/" target="_blank"><img src="https://trans-control.okdesk.ru/favicon.ico" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;width: 31px;"></a>';
 menu.appendChild(okdesk_link);
 
-let okdesk_link = document.createElement('span');
-okdesk_link.innerHTML = '<a href="https://www.facebook.com/TransControlService/" target="_blank"><img src="https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;width: 31px;"></a>';
-menu.appendChild(okdesk_link);
+let fb_link = document.createElement('span');
+fb_link.innerHTML = '<a href="https://www.facebook.com/TransControlService/" target="_blank"><img src="https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;width: 31px;"></a>';
+menu.appendChild(fb_link);
