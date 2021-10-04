1a124f5e7fd492330aadc9cf2a4ae9a963576b5f
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-04 14:31:47 +0300

    Update loggedout.js

--- loggedout.js
+++ loggedout.js
@@ -2,9 +2,9 @@ menu = document.getElementsByClassName('appstore-buttons')[0];
 
 
 let telegram_link = document.createElement('span');
-telegram_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt=""></a>';
+telegram_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;"></a>';
 menu.appendChild(telegram_link);
 
 let okdesk_link = document.createElement('span');
-okdesk_link.innerHTML = '<a href="https://trans-control.okdesk.ru/" target="_blank"><img src="https://trans-control.okdesk.ru/favicon.ico" alt=""></a>';
+okdesk_link.innerHTML = '<a href="https://trans-control.okdesk.ru/" target="_blank"><img src="https://trans-control.okdesk.ru/favicon.ico" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;"></a>';
 menu.appendChild(okdesk_link);
