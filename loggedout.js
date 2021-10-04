017c2cca7083e32f9db3afe78612886f2ee52fac
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-04 14:19:58 +0300

    Update loggedout.js

--- loggedout.js
+++ loggedout.js
@@ -1,1 +1,10 @@
+menu = document.getElementsByClassName('appstore-buttons')[0];
 
+
+let telegram_link = document.createElement('li');
+telegram_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt=""></a>';
+menu.appendChild(telegram_link);
+
+let okdesk_link = document.createElement('li');
+okdesk_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt=""></a>';
+menu.appendChild(okdesk_link);
