3a12a6d1110256015f2b346b264ab85606137644
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-04 14:22:49 +0300

    Update loggedout.js

--- loggedout.js
+++ loggedout.js
@@ -1,10 +1,10 @@
 menu = document.getElementsByClassName('appstore-buttons')[0];
 
 
-let telegram_link = document.createElement('li');
+let telegram_link = document.createElement('span');
 telegram_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt=""></a>';
 menu.appendChild(telegram_link);
 
-let okdesk_link = document.createElement('li');
-okdesk_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt=""></a>';
+let okdesk_link = document.createElement('span');
+okdesk_link.innerHTML = '<a href="https://trans-control.okdesk.ru/" target="_blank"><img src="https://trans-control.okdesk.ru/favicon.ico" alt=""></a>';
 menu.appendChild(okdesk_link);
