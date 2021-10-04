8b647c62ca14d70d3e4dd5d0dc54ee192fb1bc41
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-04 17:41:37 +0300

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -1,14 +1,25 @@
-menu = document.getElementById('sub_dom_f5acebef_3');
-menu_ul = menu.firstChild;
+(function () {
+  try  {
 
-let separator = document.createElement('li');
-separator.innerHTML = '<li class="menu-separator-top"></li>'
-menu_ul.appendChild(separator)
+      menu = document.getElementById('sub_dom_f5acebef_3');
+      menu_ul = menu.firstChild;
 
-let telegram_link = document.createElement('li');
-telegram_link.innerHTML = '<li><a target="_blank" href="https://t.me/tcs_okdesk_bot"><span dir="auto" class="menuname"><span style="background-image: url(\'https://telegram.org/favicon.ico?3\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Telegram</span></span></a></li>';
-menu_ul.appendChild(telegram_link);
+      let separator = document.createElement('li');
+      separator.innerHTML = '<li class="menu-separator-top"></li>'
+      menu_ul.appendChild(separator)
 
-let okdesk_link = document.createElement('li');
-okdesk_link.innerHTML = '<li><a target="_blank" href="https://trans-control.okdesk.ru/"><span dir="auto" class="menuname"><span style="background-image: url(\'https://trans-control.okdesk.ru/favicon.ico\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>OKDESK</span></span></a></li>';
-menu_ul.appendChild(okdesk_link);
+      let telegram_link = document.createElement('li');
+      telegram_link.innerHTML = '<li><a target="_blank" href="https://t.me/tcs_okdesk_bot"><span dir="auto" class="menuname"><span style="background-image: url(\'https://telegram.org/favicon.ico?3\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Telegram</span></span></a></li>';
+      menu_ul.appendChild(telegram_link);
+
+      let okdesk_link = document.createElement('li');
+      okdesk_link.innerHTML = '<li><a target="_blank" href="https://trans-control.okdesk.ru/"><span dir="auto" class="menuname"><span style="background-image: url(\'https://trans-control.okdesk.ru/favicon.ico\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>OKDESK</span></span></a></li>';
+      menu_ul.appendChild(okdesk_link);
+    
+    }
+  catch (err) {
+      // do nothing
+
+    })()
+
+
