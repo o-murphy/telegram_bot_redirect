68d4d57027748eb87a61c330671d50804492a7b9
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-04 17:40:44 +0300

    Update loggedout.js

--- loggedout.js
+++ loggedout.js
@@ -1,44 +1,22 @@
 (function () {
-	// make sure that if an error occurs, it doesn't break other scripts on the page
-	try	{
-		//create array with icons (path, redirectURl)
-		var icons = [
-			{
-				src:'http://dinacheley.com/wialon/lessons/images/twitter-clr.svg',
-				href:'https://twitter.com/WialonGPS'
-			},
-			{
-				src:'http://dinacheley.com/wialon/lessons/images/facebook-clr.svg',
-				href:'https://web.facebook.com/Gurtam.en'
-			},
-			{
-				src:'http://dinacheley.com/wialon/lessons/images/insta-clr.svg',
-				href:'https://www.instagram.com/gurtam_team/'
-			},
-			{
-				src:'http://dinacheley.com/wialon/lessons/images/apple-clr.svg',
-				href:'https://apps.apple.com/ru/app/id960699792'
-			}
+  try  {
 
-		];
+      menu = document.getElementsByClassName('appstore-buttons')[0];
 
-		var div = document.createElement('div');
-		icons.forEach(function(icon){
-			div.innerHTML += "<a href='"+ icon.href +"' target='_blank'><img src='"+ icon.src +"'></a>";
-		})
-		div.style = "position: absolute;" +
-			"top: 100%;" +
-			"width: 100%;" +
-			"padding: 2em 0;" +
-			"text-align: center";
 
-		document
-			.getElementsByClassName('login-bg')[0]
-			.insertAdjacentElement('beforeend', div);
-	}
-	catch (err) {
-		// do nothing
-	
-	}
+      let telegram_link = document.createElement('span');
+      telegram_link.innerHTML = '<a href="https://t.me/tcs_okdesk_bot" target="_blank"><img src="https://telegram.org/favicon.ico?3" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;width: 31px;"></a>';
+      menu.appendChild(telegram_link);
 
-})()
+      let okdesk_link = document.createElement('span');
+      okdesk_link.innerHTML = '<a href="https://trans-control.okdesk.ru/" target="_blank"><img src="https://trans-control.okdesk.ru/favicon.ico" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;width: 31px;"></a>';
+      menu.appendChild(okdesk_link);
+
+      let fb_link = document.createElement('span');
+      fb_link.innerHTML = '<a href="https://www.facebook.com/TransControlService/" target="_blank"><img src="https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico" alt="" style="background-color: white;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;border-top-left-radius: 20%;border-top-right-radius: 20%;padding: 0.3em;width: 31px;"></a>';
+      menu.appendChild(fb_link);
+    }
+  catch (err) {
+      // do nothing
+
+    })()
