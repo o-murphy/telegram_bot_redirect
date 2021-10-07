78189365a27bb5ecbffeae9d51f39c55369003ed
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-07 14:56:31 +0300

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -7,23 +7,18 @@ loader.push(function () {
             menu = document.getElementById('sub_dom_f5acebef_3');
             menu_ul = menu.firstChild;
 
-            let separator = document.createElement('li');
-            separator.innerHTML = '<li class="menu-separator-top"></li>'
-            menu_ul.appendChild(separator)
+            elements = [
+                '<li class="menu-separator-top"></li>',
+                '<a target="_blank" href="https://t.me/tcs_okdesk_bot"><span dir="auto" class="menuname"><span style="background-image: url(\'https://telegram.org/favicon.ico?3\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Telegram</span></span></a>',
+                '<a id="okd_auth"><span dir="auto" class="menuname"><span style="background-image: url(\'https://trans-control.okdesk.ru/favicon.ico\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>OKDESK</span></span></a>',
+                '<a id="cicada_tools"><span dir="auto" class="menuname"><span style="background-image: url(\'https://bitrek.ua/image/data/icon/favicon.png\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Cicada Tools</span></span></a>'
+            ]
 
-            let telegram_link = document.createElement('li');
-            telegram_link.innerHTML = '<a target="_blank" href="https://t.me/tcs_okdesk_bot"><span dir="auto" class="menuname"><span style="background-image: url(\'https://telegram.org/favicon.ico?3\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Telegram</span></span></a>';
-            menu_ul.appendChild(telegram_link);
-
-            let okdesk_link = document.createElement('li');
-            okdesk_link.innerHTML = '<a id="okd_auth"><span dir="auto" class="menuname"><span style="background-image: url(\'https://trans-control.okdesk.ru/favicon.ico\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>OKDESK</span></span></a>';
-            menu_ul.appendChild(okdesk_link);
-
-            let cicada_tools = document.createElement('li');
-            cicada_tools.innerHTML = '<a id="cicada_tools"><span dir="auto" class="menuname"><span style="background-image: url(\'https://bitrek.ua/image/data/icon/favicon.png\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Cicada Tools</span></span></a>';
-            menu_ul.appendChild(cicada_tools);
-
-
+            elements.forEach(element => {
+                let el = document.createElement('li');
+                el.innerHTML = element
+                menu_ul.appendChild(el)
+            })
 
             var onct = function onCicadaTools() {
                 var output = {};
