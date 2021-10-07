c04e2cd2d02dc80f108eeeff2f12782133e69997
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-07 16:57:37 +0300

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -6,12 +6,18 @@ loader.push(function () {
 
             menu = document.getElementById('sub_dom_f5acebef_3');
             menu_ul = menu.firstChild;
+            
+            menu_support = document.getElementById('sub_dom_f5acebef_3_5');
+
+            new_menu_support = document.createElement('a')
+            new_menu_support.setAttribute('id', 'okd_login')
+            new_menu_support.innerHTML = '<span class="menuname" style="background-image: url(\'https://trans-control.okdesk.ru/favicon.ico\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span class="menuname">Техподдержка</span>'
+            menu_support.replaceWith(new_menu_support)
 
             elements = [
                 '<li class="menu-separator-top"></li>',
-                '<a target="_blank" href="https://t.me/tcs_okdesk_bot"><span dir="auto" class="menuname"><span style="background-image: url(\'https://telegram.org/favicon.ico?3\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Telegram</span></span></a>',
-                '<a id="okd_auth"><span dir="auto" class="menuname"><span style="background-image: url(\'https://trans-control.okdesk.ru/favicon.ico\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>OKDESK</span></span></a>',
-                '<a id="cicada_tools"><span dir="auto" class="menuname"><span style="background-image: url(\'https://bitrek.ua/image/data/icon/favicon.png\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span>Cicada Tools</span></span></a>'
+                '<a target="_blank" href="https://t.me/tcs_okdesk_bot"><span class="menuname" style="background-image: url(\'https://telegram.org/favicon.ico?3\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span class="menuname">Telegram</span></a>',
+                '<a id="cicada_tools"><span class="menuname" style="background-image: url(\'https://bitrek.ua/image/data/icon/favicon.png\');display: inline-block;height: 14px;width: 14px;background-size: cover;margin-right: 0.5rem"></span><span class="menuname">Cicada Tools</span></a>'
             ]
 
             elements.forEach(element => {
@@ -19,23 +25,6 @@ loader.push(function () {
                 el.innerHTML = element
                 menu_ul.appendChild(el)
             })
-
-            var onct = function onCicadaTools() {
-                var output = {};
-                document.cookie.split(/\s*;\s*/).forEach(function (pair) {
-                    pair = pair.split(/\s*=\s*/);
-                    output[pair[0]] = pair.splice(1).join('=');
-                });
-                fetch(`http://wialon.trans-control.com/wialon/ajax.html?svc=core/create_auth_hash&params={}&sid=${output.sessions}`)
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((data) => {
-                        window.open(`https://t.me/tcs_cicada_bot?start=${data.authHash}`)
-                    });
-            }
-
-            document.getElementById('cicada_tools').onclick = onct
 
             var onokd = function onOkdTools() {
                 var output = {};
@@ -45,7 +34,8 @@ loader.push(function () {
                 });
                 username = wialon.core.Session.getInstance().__authUser
                 baseurl = wialon.core.Session.getInstance().__appDns
-                fetch(`http://wialon.trans-control.com/wialon/ajax.html?svc=core/create_auth_hash&params={}&sid=${output.sessions}`)
+                sid = wialon.core.Session.getInstance().__sessionId
+                fetch(`http://wialon.trans-control.com/wialon/ajax.html?svc=core/create_auth_hash&params={}&sid=${sid}`)
                     .then((response) => {
                         return response.json();
                     })
@@ -55,11 +45,28 @@ loader.push(function () {
                     });
             }
 
-            document.getElementById('okd_auth').onclick = onokd
+            var onct = function onCicadaTools() {
+                var output = {};
+                document.cookie.split(/\s*;\s*/).forEach(function (pair) {
+                    pair = pair.split(/\s*=\s*/);
+                    output[pair[0]] = pair.splice(1).join('=');
+                });
+                sid = wialon.core.Session.getInstance().__sessionId
+                fetch(`http://wialon.trans-control.com/wialon/ajax.html?svc=core/create_auth_hash&params={}&sid=${sid}`)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        window.open(`https://t.me/tcs_cicada_bot?start=${data.authHash}`)
+                    });
+            }
+
+            document.getElementById('okd_login').onclick = onokd
+            document.getElementById('cicada_tools').onclick = onct
 
         }
         catch (err) {
-            console.log(err)
+            console.log(err);
         }
 
     });
