f8c516eb71625dbd640dd5039b8ea6620e98d51c
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-18 17:51:08 +0300

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -187,14 +187,14 @@ loader.push(function () {
                             }
                         }
                         
-                        let own_class = element.firstChild.firstChild.classList[0]
+                        // let own_class = element.firstChild.firstChild.classList[0]
                         
                         element.onclick = resetBat
                         element.firstChild.onmouseover = function () {
                             element.firstChild.firstChild.setAttribute('class', 'icon-remove')
                         }
                         element.firstChild.onmouseleave = function () {
-                            element.firstChild.firstChild.setAttribute('class', own_class)
+                            element.firstChild.firstChild.setAttribute('class', 'icon-battery-high')
                         }
                     }
                 });
