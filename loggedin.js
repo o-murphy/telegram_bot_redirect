5dac826fd7e0d89d24765c3b6980a654d23cce22
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-10-29 14:03:37 +0300

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -13,15 +13,18 @@ loader.push(function () {
         lang_dict = {
             'ru': {
                 'title': 'Нажмите, что бы обнулить заряд',
-                'alert': "Вы точно хотите обнулить заряд аккумулятора? Это действие нельзя отменить!"
+                'alert': 'Вы точно хотите обнулить заряд аккумулятора? Это действие нельзя отменить!',
+                'reload': 'Требуется перезагрузить страницу, чтобы новые данные появились в панели "Мониторинг". Перезагрузить?'
             },
             'uk': {
                 'title': 'Натисніть, щоб скинути заряд',
-                'alert': "Ви точно хочете скинути заряд акумулятора? Цю дію не можна скасувати!"
+                'alert': 'Ви точно хочете скинути заряд акумулятора? Цю дію не можна скасувати!',
+                'reload': 'Потрібно оновити сторінку, аби нові дані з\'явились у панелі "Моніторинг". Оновити?'
             },
             'en': {
                 'title': 'Click, to reset battery charge',
-                'alert': "Are you sure you want to reset your battery? This action cannot be undone!"
+                'alert': "Are you sure you want to reset your battery? This action cannot be undone!",
+                'reload': 'Page need to be update to reload data on "Monitoring" tab. Update?'
             },
         }
 
