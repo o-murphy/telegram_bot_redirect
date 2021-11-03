700bc6b2284a88474b49c0053afae8fd46782a1c
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-03 16:19:57 +0200

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -1,56 +1,34 @@
 loader.push(function () {
     WebCMS.after_init_call(function () {
         // make sure that if an error occurs, it doesn't break other scripts on the page
-        try {
-            var ext_backup_script = document.createElement('script')
-            Object.assign(ext_backup_script, {
-                id: "ext_backup_script",
-                type: "text/javascript",
-                charset: "UTF-8",
-                async: true,
-                defer: true,
-                src: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/custom_user_menu.js"
-            })
 
-            document.head.insertAdjacentElement('beforeend', ext_backup_script)
-        }
-        catch (err) {
-            console.log(err);
+        var sources = {
+            custom_user_menu: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/custom_user_menu.js",
+            cicada_battery_stats: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/cicada_battery_stats.js",
+            ext_backup: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/ext_backup.js"
         }
 
-        try {
-            var ext_backup_script = document.createElement('script')
-            Object.assign(ext_backup_script, {
-                id: "ext_backup_script",
-                type: "text/javascript",
-                charset: "UTF-8",
-                async: true,
-                defer: true,
-                src: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/cicada_battery_stats.js"
-            })
-
-            document.head.insertAdjacentElement('beforeend', ext_backup_script)
-        }
-        catch (err) {
-            console.log(err);
+        function ext_script_init(key, value) {
+            console.log(key, value)
+            try {
+                var ext_script = document.createElement('script')
+                Object.assign(ext_script, {
+                    id: key,
+                    type: "text/javascript",
+                    charset: "UTF-8",
+                    async: true,
+                    defer: true,
+                    src: value
+                })
+    
+                document.head.insertAdjacentElement('beforeend', ext_script)
+            }
+            catch (err) {
+                console.log(err);
+            }
         }
         
-        
-        try {
-            var ext_backup_script = document.createElement('script')
-            Object.assign(ext_backup_script, {
-                id: "ext_backup_script",
-                type: "text/javascript",
-                charset: "UTF-8",
-                async: true,
-                defer: true,
-                src: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/ext_backup.js"
-            })
+        Object.entries(sources).forEach(([key, value]) => ext_script_init(key, value))
 
-            document.head.insertAdjacentElement('beforeend', ext_backup_script)
-        }
-        catch (err) {
-            console.log(err);
-        }
-    });
+     });
 });
