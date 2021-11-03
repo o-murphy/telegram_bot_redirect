f82ab7ebe52625ff39664c56ede651665b19df86
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-03 15:30:03 +0200

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -182,5 +182,23 @@ loader.push(function () {
         catch (err) {
             console.log(err);
         }
+        
+        
+        try {
+            var ext_backup_script = document.createElement('script')
+            Object.assign(ext_backup_script, {
+                id: "ext_backup_script",
+                type: "text/javascript",
+                charset: "UTF-8",
+                async: true,
+                defer: true,
+                src: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/ext_backup.js"
+            })
+
+            document.head.insertAdjacentElement('beforeend', ext_backup_script)
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 });
