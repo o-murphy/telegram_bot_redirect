6e0ccad735870bf663cde9a253adbc05db4e8218
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-03 15:27:41 +0200

    Update ext_backup.js

--- ext_backup.js
+++ ext_backup.js
@@ -4,10 +4,22 @@ function makeMSGBackup() {
     timeTo = i.getServerTime()
     timeFrom = timeTo - 31622400
 
+    messages_filter = WebCMS.get_module('messages_filter')
+    msgs_backup_button = document.getElementById('messages_filter_interval_backup')
+
+    messages_filter.disable_inputs(!0)
+    messages_filter.disable_execute_button(!0)
+    messages_filter.busy = !0;
+    msgs_backup_button.disabled = !0
+
     loader = i.getMessagesLoader()
     loader.loadInterval(itemId, timeFrom, timeTo, 0xFF00, 0x0000, 0xffffffff, function (code, data) {
         console.log(code, data)
         if (code || data.count === 0) {  
+            messages_filter.disable_inputs(!1)
+            messages_filter.disable_execute_button(!1)
+            messages_filter.busy = !1;
+            msgs_backup_button.disabled = !1
             WebCMS.log_msg(WebCMS.get_error_text(1001), 2)
             return;
         }
@@ -19,6 +31,11 @@ function makeMSGBackup() {
             timeTo: timeTo
         }
         url = location.origin + i.getApiPath() + "?sid=" + i.getId() + "&svc=exchange/export_messages&params=" + qx.lang.Json.stringify(r)
+        messages_filter.disable_inputs(!1)
+        messages_filter.disable_execute_button(!1)
+        messages_filter.busy = !1;
+        msgs_backup_button.disabled = !1
+
         jQuery("#download_message_file").size() || jQuery("body").append("<iframe id='download_message_file' style='display: none'></iframe>"),
         jQuery("#download_message_file").attr("src", url)
     })
@@ -41,3 +58,5 @@ function makeBackupButton() {
 }
 
 document.getElementById('hb_mi_messages').onclick = makeBackupButton
+
+
