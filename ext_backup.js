9d73e1b12b511286c15038703cacb60d39ddd9ba
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-03 14:58:53 +0200

    Create ext_backup.js

---
+++ ext_backup.js
@@ -0,0 +1,43 @@
+function makeMSGBackup() {
+    i = wialon.core.Session.getInstance();
+    itemId = document.getElementById("messages_filter_units").getVTBValue()
+    timeTo = i.getServerTime()
+    timeFrom = timeTo - 31622400
+
+    loader = i.getMessagesLoader()
+    loader.loadInterval(itemId, timeFrom, timeTo, 0xFF00, 0x0000, 0xffffffff, function (code, data) {
+        console.log(code, data)
+        if (code || data.count === 0) {  
+            WebCMS.log_msg(WebCMS.get_error_text(1001), 2)
+            return;
+        }
+        var r = {
+            format: 'wlb',
+            itemId: itemId,
+            compress: 1,
+            timeFrom: timeFrom,
+            timeTo: timeTo
+        }
+        url = location.origin + i.getApiPath() + "?sid=" + i.getId() + "&svc=exchange/export_messages&params=" + qx.lang.Json.stringify(r)
+        jQuery("#download_message_file").size() || jQuery("body").append("<iframe id='download_message_file' style='display: none'></iframe>"),
+        jQuery("#download_message_file").attr("src", url)
+    })
+}
+
+function makeBackupButton() {
+    filter = document.getElementById('messages_filter_interval_backup')
+    if (!filter) {
+        var msgs_backup_button = document.createElement('input')
+        Object.assign(msgs_backup_button, {
+            className: 'wui-button _min-w-button _mb-2',
+            type: 'button',
+            id: 'messages_filter_interval_backup',
+            value: 'Backup',
+            onclick: makeMSGBackup
+        })
+        msgs_buttons = document.getElementsByClassName('msgs-filter-table-buttons')
+        msgs_buttons[0].insertAdjacentElement('afterbegin', msgs_backup_button)
+    }
+}
+
+document.getElementById('hb_mi_messages').onclick = makeBackupButton
