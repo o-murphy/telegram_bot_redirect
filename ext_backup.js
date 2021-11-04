9de14ba7788fc10baf810ff6a0d8455a4e7f720b
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-04 12:49:29 +0200

    Update ext_backup.js

--- ext_backup.js
+++ ext_backup.js
@@ -1,45 +1,47 @@
 try {
+    function backupBusy(is_true) {
+        messages_filter = WebCMS.get_module('messages_filter')
+        msgs_backup_button = document.getElementById('messages_filter_interval_backup')
+
+        messages_filter.disable_inputs(is_true)
+        messages_filter.disable_execute_button(is_true)
+        messages_filter.busy = is_true
+        msgs_backup_button.disabled = is_true
+    }
+
     function makeMSGBackup() {
         i = wialon.core.Session.getInstance();
         itemId = document.getElementById("messages_filter_units").getVTBValue()
         timeTo = i.getServerTime()
         timeFrom = timeTo - 31622400
+        backupBusy(!0)
+        var r = {
+            format: 'wlb',
+            itemId: itemId,
+            compress: 1,
+            timeFrom: timeFrom,
+            timeTo: timeTo
+        }
+        url = location.origin + i.getApiPath() + "?sid=" + i.getId() + "&svc=exchange/export_messages&params=" + qx.lang.Json.stringify(r)
 
-        messages_filter = WebCMS.get_module('messages_filter')
-        msgs_backup_button = document.getElementById('messages_filter_interval_backup')
+        function download(url, filename) {
+            fetch(url, {
+                mode: 'no-cors' 
+            }).then((transfer) => {
+                return transfer.blob();                 // RETURN DATA TRANSFERED AS BLOB
+            }).then((bytes) => {
+                let elm = document.createElement('a');  // CREATE A LINK ELEMENT IN DOM
+                elm.href = URL.createObjectURL(bytes);  // SET LINK ELEMENTS CONTENTS
+                elm.setAttribute('download', filename); // SET ELEMENT CREATED 'ATTRIBUTE' TO DOWNLOAD, FILENAME PARAM AUTOMATICALLY
+                elm.click()                             // TRIGGER ELEMENT TO DOWNLOAD
+                backupBusy(!1)
+            }).catch((error) => {
+                console.log(error);                     // OUTPUT ERRORS, SUCH AS CORS WHEN TESTING NON LOCALLY
+                backupBusy(!1)
+            })
+        }
 
-        messages_filter.disable_inputs(!0)
-        messages_filter.disable_execute_button(!0)
-        messages_filter.busy = !0;
-        msgs_backup_button.disabled = !0
-
-        loader = i.getMessagesLoader()
-        loader.loadInterval(itemId, timeFrom, timeTo, 0xFF00, 0x0000, 0xffffffff, function (code, data) {
-            console.log(code, data)
-            if (code || data.count === 0) {  
-                messages_filter.disable_inputs(!1)
-                messages_filter.disable_execute_button(!1)
-                messages_filter.busy = !1;
-                msgs_backup_button.disabled = !1
-                WebCMS.log_msg(WebCMS.get_error_text(1001), 2)
-                return;
-            }
-            var r = {
-                format: 'wlb',
-                itemId: itemId,
-                compress: 1,
-                timeFrom: timeFrom,
-                timeTo: timeTo
-            }
-            url = location.origin + i.getApiPath() + "?sid=" + i.getId() + "&svc=exchange/export_messages&params=" + qx.lang.Json.stringify(r)
-            messages_filter.disable_inputs(!1)
-            messages_filter.disable_execute_button(!1)
-            messages_filter.busy = !1;
-            msgs_backup_button.disabled = !1
-
-            jQuery("#download_message_file").size() || jQuery("body").append("<iframe id='download_message_file' style='display: none'></iframe>"),
-            jQuery("#download_message_file").attr("src", url)
-        })
+        download(url, itemId)        
     }
 
     function makeBackupButton() {
