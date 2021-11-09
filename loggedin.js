a4a53ff34ded8cde266e303b2a416b4b4082b2b1
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-09 13:08:53 +0200

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -2,33 +2,40 @@ loader.push(function () {
     WebCMS.after_init_call(function () {
         // make sure that if an error occurs, it doesn't break other scripts on the page
 
-        var sources = {
-            lang_dict: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/lang_dict.js",
-            custom_user_menu: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/custom_user_menu.js",
-            cicada_battery_stats: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/cicada_battery_stats.js",
-            ext_backup: "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect@main/ext_backup.js"
-        }
-
-        function ext_script_init(key, value) {
-            try {
-                var ext_script = document.createElement('script')
-                Object.assign(ext_script, {
-                    id: key,
-                    type: "text/javascript",
-                    charset: "UTF-8",
-                    async: true,
-                    defer: true,
-                    src: value
-                })
-    
-                document.head.insertAdjacentElement('beforeend', ext_script)
+        fetch('https://api.github.com/repos/o-murphy/telegram_bot_redirect/branches/main').then(
+            (data) => {
+                return data.json()
             }
-            catch (err) {
-                console.log(err);
+        ).then((json) => {
+            sha = json.commit.sha
+            var cdn = "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect"
+            sources = {
+                lang_dict: `${cdn}@${sha}/lang_dict.js`,
+                custom_user_menu: `${cdn}@${sha}/custom_user_menu.js`,
+                cicada_battery_stats: `${cdn}@${sha}/cicada_battery_stats.js`,
+                ext_backup: `${cdn}@${sha}/ext_backup.js`
             }
-        }
-        
-        Object.entries(sources).forEach(([key, value]) => ext_script_init(key, value))
 
-     });
+            function ext_script_init(key, value) {
+                try {
+                    var ext_script = document.createElement('script')
+                    Object.assign(ext_script, {
+                        id: key,
+                        type: "text/javascript",
+                        charset: "UTF-8",
+                        async: true,
+                        defer: true,
+                        src: value
+                    })
+
+                    document.head.insertAdjacentElement('beforeend', ext_script)
+                }
+                catch (err) {
+                    console.log(err);
+                }
+            }
+
+            Object.entries(sources).forEach(([key, value]) => ext_script_init(key, value))
+        })
+    });
 });
