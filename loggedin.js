bbfa649e1f19818d24e90d8dfd28052040f3863f
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-11-10 13:13:42 +0200

    Update loggedin.js

--- loggedin.js
+++ loggedin.js
@@ -1,7 +1,5 @@
 loader.push(function () {
     WebCMS.after_init_call(function () {
-        // make sure that if an error occurs, it doesn't break other scripts on the page
-
         fetch('https://api.github.com/repos/o-murphy/telegram_bot_redirect/branches/main').then(
             (data) => {
                 return data.json()
@@ -9,12 +7,7 @@ loader.push(function () {
         ).then((json) => {
             sha = json.commit.sha
             var cdn = "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect"
-            sources = {
-                lang_dict: `${cdn}@${sha}/lang_dict.js`,
-                custom_user_menu: `${cdn}@${sha}/custom_user_menu.js`,
-                cicada_battery_stats: `${cdn}@${sha}/cicada_battery_stats.js`,
-                ext_backup: `${cdn}@${sha}/ext_backup.js`
-            }
+            url = `${cdn}@${sha}/ext_init.js`
 
             function ext_script_init(key, value) {
                 try {
@@ -34,8 +27,7 @@ loader.push(function () {
                     console.log(err);
                 }
             }
-
-            Object.entries(sources).forEach(([key, value]) => ext_script_init(key, value))
-        })
+            ext_script_init('ext_init', url);
+        });
     });
 });
