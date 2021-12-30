deb14e57bb91b7871918a0b7ef63a1cb60431e8d
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-12-31 01:07:28 +0200

    Update ext_init.js

--- ext_init.js
+++ ext_init.js
@@ -1,17 +1,25 @@
 try {
 
-          fetch('https://api.github.com/repos/o-murphy/telegram_bot_redirect/branches/main').then(
-            (data) => {
-                return data.json()
-            }
-          ).then((json) => {
-            sha = json.commit.sha
-            var cdn = "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect"
+//           fetch('https://api.github.com/repos/o-murphy/telegram_bot_redirect/branches/main').then(
+//             (data) => {
+//                 return data.json()
+//             }
+//           ).then((json) => {
+//             sha = json.commit.sha
+//             var cdn = "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect"
+            var base_url = "https://o-murphy.github.io/telegram_bot_redirect"
+//             sources = {
+//                 lang_dict: `${cdn}@${sha}/lang_dict.js`,
+//                 custom_user_menu: `${cdn}@${sha}/custom_user_menu.js`,
+//                 cicada_battery_stats: `${cdn}@${sha}/cicada_battery_stats.js`,
+//                 ext_backup: `${cdn}@${sha}/ext_backup.js`,
+//                 additional: ``
+//             }
             sources = {
-                lang_dict: `${cdn}@${sha}/lang_dict.js`,
-                custom_user_menu: `${cdn}@${sha}/custom_user_menu.js`,
-                cicada_battery_stats: `${cdn}@${sha}/cicada_battery_stats.js`,
-                ext_backup: `${cdn}@${sha}/ext_backup.js`,
+                lang_dict: `${base_url}/lang_dict.js`,
+                custom_user_menu: `${base_url}/custom_user_menu.js`,
+                cicada_battery_stats: `${base_url}/cicada_battery_stats.js`,
+                ext_backup: `${base_url}/ext_backup.js`,
                 additional: ``
             }
 
