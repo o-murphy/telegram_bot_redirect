b4051c0f8bfc5c01ebcb2f00e750c62cf3a9f66a
o-murphy <73843436+o-murphy@users.noreply.github.com>
2021-12-31 01:13:47 +0200

    Update ext_init.js

--- ext_init.js
+++ ext_init.js
@@ -1,20 +1,5 @@
 try {
-
-//           fetch('https://api.github.com/repos/o-murphy/telegram_bot_redirect/branches/main').then(
-//             (data) => {
-//                 return data.json()
-//             }
-//           ).then((json) => {
-//             sha = json.commit.sha
-//             var cdn = "https://cdn.jsdelivr.net/gh/o-murphy/telegram_bot_redirect"
             var base_url = "https://o-murphy.github.io/telegram_bot_redirect"
-//             sources = {
-//                 lang_dict: `${cdn}@${sha}/lang_dict.js`,
-//                 custom_user_menu: `${cdn}@${sha}/custom_user_menu.js`,
-//                 cicada_battery_stats: `${cdn}@${sha}/cicada_battery_stats.js`,
-//                 ext_backup: `${cdn}@${sha}/ext_backup.js`,
-//                 additional: ``
-//             }
             sources = {
                 lang_dict: `${base_url}/lang_dict.js`,
                 custom_user_menu: `${base_url}/custom_user_menu.js`,
@@ -22,7 +7,6 @@ try {
                 ext_backup: `${base_url}/ext_backup.js`,
                 additional: ``
             }
-
             function ext_script_init(key, value) {
                 if (value) {
                           try {
@@ -45,6 +29,5 @@ try {
             }
 
             Object.entries(sources).forEach(([key, value]) => ext_script_init(key, value))
-          })
           
 } catch (error) {console.log(error)}
