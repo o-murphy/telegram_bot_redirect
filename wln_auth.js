69a24cec1cedbb5c6c5fbf773254670086dd3870
o-murphy <73843436+o-murphy@users.noreply.github.com>
2022-01-13 12:15:10 +0200

    Update wln_auth.js

--- wln_auth.js
+++ wln_auth.js
@@ -3,10 +3,10 @@ function wln_auth (url, key) {
     s.getAccountData(
         s.__currUser.$$user_accountId, 
         function(code, data) {
-            if (code) msg(wialon.core.Errors.getErrorText(code))
+            if (code) console.log(wialon.core.Errors.getErrorText(code))
             else if (data.dealerRights === 1) {
                 s.createAuthHash(function(code, data) {
-                    if (code) msg(wialon.core.Errors.getErrorText(code))
+                    if (code) console.log(wialon.core.Errors.getErrorText(code))
                     else {
                         window.open(encodeURI(`${url}?baseurl=${s.__appDns}&${key}=${data.authHash}`))
                     }
