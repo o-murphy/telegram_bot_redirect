f9c13886a37b53d3123e6d0ba288a467a729c651
o-murphy <73843436+o-murphy@users.noreply.github.com>
2022-02-06 22:13:13 +0200

    Update ext_init.js

--- ext_init.js
+++ ext_init.js
@@ -2,11 +2,11 @@ try {
             var base_url = "https://o-murphy.github.io/telegram_bot_redirect"
             sources = {
                 lang_dict: `${base_url}/lang_dict.js`,
-                custom_user_menu: `${base_url}/custom_user_menu.js`,
-                cicada_battery_stats: `${base_url}/cicada_battery_stats.js`,
-                ext_backup: `${base_url}/ext_backup.js`,
-                additional: ``,
-                wln_auth: `${base_url}/wln_auth.js`
+//                 custom_user_menu: `${base_url}/custom_user_menu.js`,
+//                 cicada_battery_stats: `${base_url}/cicada_battery_stats.js`,
+//                 ext_backup: `${base_url}/ext_backup.js`,
+//                 additional: ``,
+//                 wln_auth: `${base_url}/wln_auth.js`
             }
             function ext_script_init(key, value) {
                 if (value) {
